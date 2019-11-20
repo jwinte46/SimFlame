07e581ef1104b35fc0f7248d4c8a4836c55e516e
Tim Hansen <heroofgraz@gmail.com>
2019-11-20 15:59:22 -0700

    Added semicolon

--- src/csis1410/SimFlame/Main.java
+++ src/csis1410/SimFlame/Main.java
@@ -17,7 +17,7 @@ public class Main {
    public static void main(String[] args) {
       World world = new World(100, 100);
       Simulation simulation = new Simulation(world);
-      Window window = new Window(simulation)
+      Window window = new Window(simulation);
    }
    
 }
