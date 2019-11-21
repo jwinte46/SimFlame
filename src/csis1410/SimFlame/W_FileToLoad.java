f3ebc1cf628a150e775206ba2f6b5303e4f3dc1b
Tim Hansen <heroofgraz@gmail.com>
2019-11-20 19:26:29 -0700

    Changed JTextFields to JLabels, decreased vertical size of file windows

--- src/csis1410/SimFlame/W_FileToLoad.java
+++ src/csis1410/SimFlame/W_FileToLoad.java
@@ -24,7 +24,7 @@ public class W_FileToLoad extends JFrame {
 	 * Create the frame.
 	 */
 	public W_FileToLoad(Simulation simulation) {//Capitalized: Name of a type. Uncapitalized: Name of the variable
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- src/csis1410/SimFlame/W_FileToSave.java
+++ src/csis1410/SimFlame/W_FileToSave.java
@@ -9,11 +9,12 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.JLabel;
 
 public class W_FileToSave extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtWhichFileDo;
+	private JLabel txtWhichFileDo;
 	private JTextField textField;
 
 
@@ -22,17 +23,16 @@ public class W_FileToSave extends JFrame {
 	 */
 	public W_FileToSave(Simulation simulation) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 442, 83);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		txtWhichFileDo = new JTextField();
+		txtWhichFileDo = new JLabel();
 		txtWhichFileDo.setHorizontalAlignment(SwingConstants.CENTER);
 		txtWhichFileDo.setText("Which file do you want to save?");
 		contentPane.add(txtWhichFileDo, BorderLayout.NORTH);
-		txtWhichFileDo.setColumns(10);
 		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.EAST);
